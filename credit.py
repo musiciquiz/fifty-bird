2b177ea1aa1617b1b3c9d1bc9af1e9e91b19c76f
musiciquiz <musiciquiz@users.noreply.github.com>
2020-02-29 08:33:54 +0000

    automated commit by submit50

--- credit.py
+++ credit.py
@@ -3,7 +3,7 @@ from cs50 import get_int
 
 # prompt input
 card = get_int("Number: ")
-#calculate checksum
+# calculate checksum
 # initiate global variables
 
 counter_even = 10
@@ -14,17 +14,17 @@ sum_odd = 0
 i = 100
 while True:
     x = int((card % i) / counter_even)
-    counter_even *=100
-    x *=2
-    #what if double digit number?
+    counter_even *= 100
+    x *= 2
+    # what if double digit number?
     if x > 9:
         x = (x % 10) + 1
     sum_even += x
     i *= 100
     if i > 55999999999999999:
-         break
+        break
 
-#finding sum odd numbers with counter j
+# finding sum odd numbers with counter j
 j = 10
 while True:
     x = int((card % j) / counter_odd)
@@ -41,7 +41,7 @@ checksum = sum_even + sum_odd
 if (card >= 4000000000000 and card < 5000000000000 and checksum % 10 == 0):
     print("VISA")
 
-#VISA - 16 digit and start with 4
+# VISA - 16 digit and start with 4
 elif (card >= 4000000000000000 and card < 5000000000000000 and checksum % 10 == 0):
     print("VISA")
 
@@ -50,7 +50,7 @@ elif (card >= 5100000000000000 and card < 5600000000000000 and checksum % 10 == 
     print("MASTERCARD")
 
 # AMEX 15 digit starts 34 or 37
-elif (card >= 340000000000000 and card < 350000000000000 and checksum % 10  == 0):
+elif (card >= 340000000000000 and card < 350000000000000 and checksum % 10 == 0):
     print("AMEX")
 
 elif (card >= 370000000000000 and card < 380000000000000 and checksum % 10 == 0):
@@ -58,10 +58,3 @@ elif (card >= 370000000000000 and card < 380000000000000 and checksum % 10 == 0)
 
 else:
     print("INVALID")
-
-
-
-
-
-
-
